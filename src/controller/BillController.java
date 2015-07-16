1c74211e526d784ce4a3b2ab774b0d95561dfe48
dayoungles <dayoungles@gmail.com>
2015-07-16 14:11:30 +0900

    schema change result

---
+++ src/controller/BillController.java
@@ -0,0 +1,62 @@
+package controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import model.Bill;
+import model.FileUpload;
+import model.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import service.BillService;
+import service.UploadService;
+
+@Controller
+@RequestMapping("bill")
+public class BillController {
+
+	@Autowired
+	BillService billService;
+	
+	@Autowired
+	UploadService uploadService;
+	
+	@RequestMapping("/form/{bagId}")
+	public String createForm(Model model, @PathVariable("bagId") String bagId) {
+		model.addAttribute("bagId", bagId);
+		
+		return "/bag/billForm";
+	}
+
+	@RequestMapping("create/{bagId}")
+	public String createRound(@PathVariable("bagId") String bagId,
+			@RequestParam("file") MultipartFile file,
+			@RequestParam("usedMoney") String usedMoney,
+			@RequestParam("info") String info, 
+			@RequestParam("billName") String billName,
+			Model model,HttpServletRequest request,  HttpSession session) {
+		User user = (User) session.getAttribute("user");
+		Bill bill = new Bill(billName, usedMoney, info, Integer.parseInt(bagId));
+		
+		
+		if(uploadService.isImgFile(file) == false){
+			return "이미지 파일만 입력 가능합니다";
+		}
+		String realPath = request.getSession().getServletContext()
+				.getRealPath("/");
+		realPath += "/bill_img/";
+		FileUpload upload = uploadService.fileSetting(file, realPath);
+		bill.setFileName(upload.getName());
+		
+		billService.createBill(bill, user);
+		return "redirect:/showBag/" + bagId;
+	}
+}
---
+++ src/dao/BillDao.java
@@ -0,0 +1,59 @@
+package dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import model.Bill;
+import model.User;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BillDao {
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(BillDao.class);
+
+	private RowMapper<Bill> billMapper = new RowMapper<Bill>() {
+		@Override
+		public Bill mapRow(ResultSet rs, int rowNum) throws SQLException {
+			Bill bill = new Bill();
+			bill.setBill_id(rs.getInt("bill_id"));
+			bill.setCreatedDate(rs.getString("createdDate"));
+			bill.setBillName(rs.getString("billName"));
+			bill.setInfo(rs.getString("info"));
+			bill.setFileName(rs.getString("fileName"));
+			bill.setUsedMoney(rs.getInt("usedMoney"));
+			return bill;
+		};
+
+	};
+
+	public List<Bill> findAllBillsByBagId(int bagId) {
+		String sql = "select * from bill where moneybagId=?";
+		try {
+			return jdbcTemplate.query(sql, new Object[] { bagId },
+					this.billMapper);
+		} catch (EmptyResultDataAccessException e) {
+			return null;
+		}
+
+	}
+
+	public void createBill(Bill bill, User user) {
+		String sql = "insert into bill (moneybagId, billName, info, fileName, usedMoney) values (?,?,?,?,?) ";
+		logger.debug("bill:{}", bill);
+		jdbcTemplate.update(sql, bill.getMoneybagId(), bill.getBillName(), bill.getInfo(),bill.getFileName(),bill.getUsedMoney());
+		
+	}
+
+}
